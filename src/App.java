d8bae28faf4cb0dc877dceb583dfa8dfafc48e93
emrecimen20 <emrecimen48@outlook.com>
2024-06-08 16:12:41 +0300

    Rent a car project first  draft push

---
+++ src/App.java
@@ -0,0 +1,11 @@
+import core.Helper;
+import view.LoginView;
+
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) {
+        Helper.setTheme();
+        LoginView loginView =new LoginView();
+    }
+}
---
+++ src/dao/UserDao.java
@@ -0,0 +1,57 @@
+package dao;
+
+import core.Db;
+import entity.User;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class UserDao {
+    private final Connection con;
+
+    public UserDao(){
+        this.con = Db.getInstance();
+    }
+
+    public ArrayList<User> findAll(){
+        ArrayList<User> userList =null;
+        String query = "SELECT * FROM public.user";
+        try {
+           Statement st =this.con.createStatement();
+           ResultSet rs =st.executeQuery(query);
+           while(rs.next()){
+               userList.add(this.match(rs));
+           }
+
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return userList;
+    }
+
+    public User findByLogin(String username, String password){
+        User obj =null;
+        String query = "SELECT * FROM public.user WHERE user_name = ? AND user_password = ?";
+        try {
+            PreparedStatement pr = this.con.prepareStatement(query);
+            pr.setString(1,username);
+            pr.setString(2,password);
+            ResultSet rs = pr.executeQuery();
+            if(rs.next()){
+              obj = this.match(rs);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return obj;
+    }
+    public User match(ResultSet rs) throws SQLException {
+        User obj = new User();
+        obj.setId(rs.getInt("user_id"));
+        obj.setUsername(rs.getString("user_name"));
+        obj.setPassword(rs.getString("user_password"));
+        obj.setRole(rs.getString("user_role"));
+        return obj;
+    }
+}
