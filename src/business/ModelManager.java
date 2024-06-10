625d0fb1e2bff2d9ee779002519f143ac608f9fb
emrecimen20 <emrecimen48@outlook.com>
2024-06-11 00:39:20 +0300

    Add CarView, CarDao, CarManager, Car entity

--- src/business/ModelManager.java
+++ src/business/ModelManager.java
@@ -9,18 +9,18 @@ import java.util.ArrayList;
 public class ModelManager {
     private final ModelDao modelDao = new ModelDao();
 
-    public Model getById(int id){
+    public Model getById(int id) {
         return this.modelDao.getById(id);
     }
 
-    public ArrayList<Model> findAll(){
+    public ArrayList<Model> findAll() {
         return this.modelDao.findAll();
     }
 
-    public ArrayList<Object[]> getForTable(int size,ArrayList<Model> modelList){
+    public ArrayList<Object[]> getForTable(int size, ArrayList<Model> modelList) {
         ArrayList<Object[]> modelObjList = new ArrayList<>();
-        for(Model obj : modelList){
-            int i =0;
+        for (Model obj : modelList) {
+            int i = 0;
             Object[] rowObject = new Object[size];
             rowObject[i++] = obj.getId();
             rowObject[i++] = obj.getBrand().getName();
@@ -34,8 +34,8 @@ public class ModelManager {
         return modelObjList;
     }
 
-    public boolean save(Model model){
-        if(this.getById(model.getId()) != null){
+    public boolean save(Model model) {
+        if (this.getById(model.getId()) != null) {
 
             Helper.showMssg("error");
             return false;
@@ -43,23 +43,48 @@ public class ModelManager {
         return this.modelDao.save(model);
     }
 
-    public boolean update(Model model){
-        if(this.getById(model.getId())==null){
+    public boolean update(Model model) {
+        if (this.getById(model.getId()) == null) {
             Helper.showMssg(model.getId() + " ID kayırlı model bulunamadı.");
             return false;
         }
         return this.modelDao.update(model);
     }
 
-    public boolean delete(int id){
-        if(this.getById(id) == null){
+    public boolean delete(int id) {
+        if (this.getById(id) == null) {
             Helper.showMssg(id + " ID kayıtlı model bulunamadı.");
             return false;
         }
         return this.modelDao.delete(id);
     }
 
-    public ArrayList<Model> getByListBrandId(int brandId){
+    public ArrayList<Model> getByListBrandId(int brandId) {
+        return this.modelDao.getByListBrandId(brandId);
+    }
+
+    public ArrayList<Model> searchForTable(int brandId, Model.Fuel fuel, Model.Gear gear, Model.Type type) {
+        String select ="SELECT * FROM public.model";
+        ArrayList<String> whereList = new ArrayList<>();
+
+        if(brandId != 0){
+            whereList.add("model_brand_id = " + brandId);
+        }
+
+        if(fuel != null){
+            whereList.add("model_fuel ='" + fuel.toString() + "'");
+        }
+        if(gear != null){
+            whereList.add("model_gear ='" + gear.toString() + "'");
+        }
+        if(type != null){
+            whereList.add("model_type ='" + type.toString() + "'");
+        }
+        String whereStr = String.join("AND",whereList);
+        String query =select;
+        if(whereStr.length() > 0){
+            query+= "WHERE " + whereStr;
+        }
         return this.modelDao.getByListBrandId(brandId);
     }
 }
--- src/view/AdminView.java
+++ src/view/AdminView.java
@@ -1,8 +1,12 @@
 package view;
 
 import business.BrandManager;
+import business.CarManager;
 import business.ModelManager;
+import core.ComboItem;
 import core.Helper;
+import entity.Brand;
+import entity.Car;
 import entity.Model;
 import entity.User;
 
@@ -16,7 +20,7 @@ public class AdminView extends Layout {
     private JPanel container;
     private JLabel lbl_welcome;
     private JPanel pnl_top;
-    private JTabbedPane tab_menu;
+    private JTabbedPane tab_menu_car;
     private JButton btn_logout;
     private JPanel pnl_brand;
     private JScrollPane scl_brand;
@@ -24,15 +28,34 @@ public class AdminView extends Layout {
     private JPanel pnl_model;
     private JScrollPane scrl_model;
     private JTable tbl_model;
+    private JLabel lbl_src_brand;
+    private JComboBox cmb_model_brand;
+    private JComboBox cmb_s_type;
+    private JComboBox cmb_s_model_fuel;
+    private JComboBox cmb_s_model_gear;
+    private JButton btn_search_model;
+    private JLabel lbl_src_type;
+    private JLabel lbl_src_fuel;
+    private JLabel lbl_src_gear;
+    private JButton btn_clear_src;
+    private JPanel pnl_car;
+    private JTable table_car;
+    private JPanel pnl_carr;
+    private JScrollPane scr_car;
     private User user;
     private DefaultTableModel tmdl_brand = new DefaultTableModel();
     private DefaultTableModel tmdl_model = new DefaultTableModel();
+    private DefaultTableModel tmdl_car =new DefaultTableModel();
     private BrandManager brandManager;
     private ModelManager modelManager;
     private JPopupMenu brand_menu;
     private JPopupMenu model_menu;
+    private JPopupMenu car_menu;
+    private Object[] col_model;
+    private CarManager carManager;
 
     public AdminView(User user) {
+        this.carManager=new CarManager();
         this.modelManager =new ModelManager();
         this.brandManager = new BrandManager();
         this.add(container);
@@ -48,9 +71,56 @@ public class AdminView extends Layout {
         loadBrandTable();
         loadBrandComponent();
 
-        loadModelTable();
-        loadModelComponent();
 
+        loadModelTable(null);
+        loadModelComponent();
+        loadModelFilter();
+
+        loadCarTable();
+        loadCarComponent();
+
+    }
+
+    public void loadCarTable(){
+        Object [] col_car ={"ID","Marka","Model","Plaka","Renk","KM","Yıl","Tip","Yakıt Türü","Vites"};
+        ArrayList<Object[]> carList = this.carManager.getForTable(col_car.length,this.carManager.findAll());
+        createTable(this.tmdl_car,this.table_car,col_car,carList);
+    }
+
+    public void loadCarComponent(){
+        this.car_menu =new JPopupMenu();
+        tableRowSelect(this.table_car,car_menu);
+
+        this.car_menu.add("Yeni").addActionListener(e -> {
+            CarView carView = new CarView(new Car());
+            carView.addWindowListener(new WindowAdapter() {
+                @Override
+                public void windowClosed(WindowEvent e) {
+                    loadCarTable();
+                }
+            });
+        });
+        this.car_menu.add("Güncelle").addActionListener(e -> {
+            int selectedModelId = this.getTableSelectedRow(table_car, 0);
+            CarView carView = new CarView(this.carManager.getById(selectedModelId));
+            carView.addWindowListener(new WindowAdapter() {
+                @Override
+                public void windowClosed(WindowEvent e) {
+                    loadCarTable();
+                }
+            });
+        });
+        this.car_menu.add("Sil").addActionListener(e -> {
+            if (Helper.confirm("sure")) {
+                int selectedModelId = this.getTableSelectedRow(table_car, 0);
+                if (this.carManager.delete(selectedModelId)) {
+                    Helper.showMssg("done");
+                    loadCarTable();
+                } else {
+                    Helper.showMssg("error");
+                }
+            }
+        });
     }
 
     private void loadModelComponent() {
@@ -62,7 +132,7 @@ public class AdminView extends Layout {
             modelView.addWindowListener(new WindowAdapter() {
                 @Override
                 public void windowClosed(WindowEvent e) {
-                    loadModelTable();
+                    loadModelTable(null);
                 }
             });
         });
@@ -72,7 +142,7 @@ public class AdminView extends Layout {
             modelView.addWindowListener(new WindowAdapter() {
                 @Override
                 public void windowClosed(WindowEvent e) {
-                    loadModelTable();
+                    loadModelTable(null);
                 }
             });
         });
@@ -81,7 +151,7 @@ public class AdminView extends Layout {
                 int selectedModelId = this.getTableSelectedRow(tbl_model, 0);
                 if (this.modelManager.delete(selectedModelId)) {
                     Helper.showMssg("done");
-                    loadModelTable();
+                    loadModelTable(null);
                 } else {
                     Helper.showMssg("error");
                 }
@@ -89,12 +159,56 @@ public class AdminView extends Layout {
         });
 
         this.tbl_model.setComponentPopupMenu(model_menu);
+
+        this.btn_search_model.addActionListener(e -> {
+            ComboItem selectedBrand = (ComboItem) this.cmb_model_brand.getSelectedItem();
+            int brandId =0;
+            if(selectedBrand != null){
+                brandId = selectedBrand.getKey();
+            }
+            ArrayList<Model> modelListBySearch = this.modelManager.searchForTable(
+                    brandId,
+                    (Model.Fuel)cmb_s_model_fuel.getSelectedItem(),
+                    (Model.Gear)cmb_s_model_gear.getSelectedItem(),
+                    (Model.Type)cmb_s_type.getSelectedItem()
+
+            );
+            ArrayList<Object[]> modelRowListBySearch = this.modelManager.getForTable(this.col_model.length,modelListBySearch);
+            loadModelTable(modelRowListBySearch);
+        });
+
+        this.btn_clear_src.addActionListener(e -> {
+            this.cmb_s_type.setSelectedItem(null);
+            this.cmb_s_model_gear.setSelectedItem(null);
+            this.cmb_s_model_fuel.setSelectedItem(null);
+            this.cmb_model_brand.setSelectedItem(null);
+            loadModelTable(null);
+        });
     }
 
-    public void loadModelTable() {
-        Object[] col_model = {"Model ID", "Marka", "Model Adı", "Tip", "Yıl", "Yakıt Türü", "Vites"};
-        ArrayList<Object[]> modelList = this.modelManager.getForTable(col_model.length,this.modelManager.findAll());
-        createTable(this.tmdl_model,this.tbl_model,col_model,modelList);
+    public void loadModelTable(ArrayList<Object[]> modelList) {
+       this.col_model = new Object[]{"Model ID", "Marka", "Model Adı", "Tip", "Yıl", "Yakıt Türü", "Vites"};
+        if(modelList == null){
+            modelList = this.modelManager.getForTable(this.col_model.length,this.modelManager.findAll());
+        }
+        createTable(this.tmdl_model,this.tbl_model,this.col_model,modelList);
+    }
+
+    public void loadModelFilter(){
+        this.cmb_s_type.setModel(new DefaultComboBoxModel<>(Model.Type.values()));
+        this.cmb_s_type.setSelectedItem(null);
+        this.cmb_s_model_gear.setModel(new DefaultComboBoxModel<>(Model.Gear.values()));
+        this.cmb_s_model_gear.setSelectedItem(null);
+        this.cmb_s_model_fuel.setModel(new DefaultComboBoxModel<>(Model.Fuel.values()));
+        this.cmb_s_model_fuel.setSelectedItem(null);
+        loadModelFilterBrand();
+    }
+    public void loadModelFilterBrand(){
+        this.cmb_model_brand.removeAllItems();
+        for(Brand obj : brandManager.findAll()){
+            this.cmb_model_brand.addItem(new ComboItem(obj.getId(),obj.getName()));
+        }
+        this.cmb_model_brand.setSelectedItem(null);
     }
 
     public void loadBrandTable() {
@@ -113,7 +227,8 @@ public class AdminView extends Layout {
                 @Override
                 public void windowClosed(WindowEvent e) {
                     loadBrandTable();
-                    loadModelTable();
+                    loadModelTable(null);
+                    loadModelFilterBrand();
                 }
             });
         });
@@ -124,7 +239,8 @@ public class AdminView extends Layout {
                 @Override
                 public void windowClosed(WindowEvent e) {
                     loadBrandTable();
-                    loadModelTable();
+                    loadModelTable(null);
+                    loadModelFilterBrand();
                 }
             });
         });
@@ -134,7 +250,8 @@ public class AdminView extends Layout {
                 if (this.brandManager.delete(selectBrandId)) {
                     Helper.showMssg("done");
                     loadBrandTable();
-                    loadModelTable();
+                    loadModelTable(null);
+                    loadModelFilterBrand();
                 } else {
                     Helper.showMssg("error");
                 }
